06cda570a031ce3a0b0718c3335ddfba3d63f2b7
Nickz097 <nicolas.r.silva@eaportal.org>
2025-03-14 11:49:53 -0300

    Trabalhando com entrada de dados

---
+++ Entrada_Joption.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane; 
+
+    public class Entrada_Joption { 
+        public static void main(String[] args) {
+String nome = JOptionPane.showInputDialog("Digite seu nome");
+
+String idade = JOptionPane.showInputDialog("Digite a sua idade");
+
+double altura = Double.parseDouble(JOptionPane.showInputDialog("Digite sua altura"));
+
+int idadeStr = Integer.parseInt(idade);
+
+JOptionPane.showMessageDialog(null,"Olá, meu nome é " + nome + " tenho " + idadeStr + " de idade, e " + altura + " de altura");
+        }
+}
+
